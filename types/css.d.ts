fface2047f0f59c1f87a7203c8b60bd67ad7b288
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2026-01-06 21:42:38 +0530

    init

---
+++ types/css.d.ts
@@ -0,0 +1,10 @@
+// Type declarations for CSS imports
+declare module '*.css' {
+    const content: { [className: string]: string };
+    export default content;
+}
+
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
