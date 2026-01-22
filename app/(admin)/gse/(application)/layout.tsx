7e10e477dbc8ec40e3922fb20b949412443b7515
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2026-01-22 21:48:20 +0530

    admin route setup

---
+++ app/(admin)/gse/(application)/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next"
+import { Montserrat } from "next/font/google"
+
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
+
+export const metadata: Metadata = {
+  title: "Global Source Export",
+  description: "A b2b market place for buyers.",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} ${montserrat.className}`}>
+
+          {children}
+
+      </body>
+    </html>
+  )
+}
